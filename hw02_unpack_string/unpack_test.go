1e35d85b0d60ad157603090974172d25707bf6c1
Фролов Максим Александрович <maksim.frolov@vseinstrumenti.ru>
2023-03-26 20:25:55 +0300

    hw02_unpack_string is complited

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -34,12 +34,23 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abxc", "75689"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackStringContainsNumbers(t *testing.T) {
+	invalidStrings := []string{"cr55xp", "rr53jhgfd"}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrStringContainsNumbers), "actual error %q", err)
 		})
 	}
 }
