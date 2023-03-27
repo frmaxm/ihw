a9edb7ecca35aece4ee0fe19953c01d8ea49f8bb
Фролов Максим Александрович <maksim.frolov@vseinstrumenti.ru>
2023-03-27 23:13:10 +0300

    hw03_frequency_analysis is complited

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,38 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordWithTotal struct {
+	word  string
+	total int
+}
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+
+	wordTotals := make(map[string]int)
+	for _, word := range words {
+		wordTotals[word]++
+	}
+
+	wordWithTotalSlice := []wordWithTotal{}
+	for word, count := range wordTotals {
+		wordWithTotalSlice = append(wordWithTotalSlice, wordWithTotal{word, count})
+	}
+
+	sort.Slice(wordWithTotalSlice, func(i, j int) bool {
+		if wordWithTotalSlice[i].total == wordWithTotalSlice[j].total {
+			return wordWithTotalSlice[i].word < wordWithTotalSlice[j].word
+		}
+		return wordWithTotalSlice[i].total > wordWithTotalSlice[j].total
+	})
+
+	var topWords []string
+	for i := 0; i < 10 && i < len(wordWithTotalSlice); i++ {
+		topWords = append(topWords, wordWithTotalSlice[i].word)
+	}
+	return topWords
 }
